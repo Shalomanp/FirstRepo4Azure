f0495d3b7835c2ec7979feb98df1097a08b1d7c5
Shalomanp <shaloman_pandit@hotmail.com>
2020-06-01 11:24:36 +0530

    Initial Version
    1.0

---
+++ FirstRepo4Azure/src/app/firstRepoTab/FirstRepoTab.ts
@@ -0,0 +1,10 @@
+import { PreventIframe } from "express-msteams-host";
+
+/**
+ * Used as place holder for the decorators
+ */
+@PreventIframe("/firstRepoTab/index.html")
+
+
+export class FirstRepoTab {
+}
---
+++ FirstRepo4Azure/src/app/scripts/client.ts
@@ -0,0 +1,2 @@
+// Automatically added for the firstRepoTab tab
+export * from "./firstRepoTab/FirstRepoTab";
